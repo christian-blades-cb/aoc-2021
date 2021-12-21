a2b1966fb1714a68e77c0fc80b177c5953af3822
Christian Blades <christian.blades+github@gmail.com>
2021-12-20 22:22:50 -0500

    day4
    in which I spent too much time messing with the parser

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use eyre::{bail, eyre, Result};
 use lazy_static::lazy_static;
+use nom::IResult;
 use regex::Regex;
 
 fn main() {
@@ -56,6 +57,176 @@ forward 2
     println!("day3pt1: {}", day3pt1(&day3input));
     println!("day3pt2ex: {}", day3pt2(&day3exinput));
     println!("day3pt2: {}", day3pt2(&day3input));
+
+    let day4exinput = parseday4(
+        r#"7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
+
+22 13 17 11  0
+ 8  2 23  4 24
+21  9 14 16  7
+ 6 10  3 18  5
+ 1 12 20 15 19
+
+ 3 15  0  2 22
+ 9 18 13 17  5
+19  8  7 25 23
+20 11 10 24  4
+14 21 16 12  6
+
+14 21 17 24  4
+10 16 15  9 19
+18  8 23 26 20
+22 11 13  6  5
+2  0 12  3  7
+"#,
+    )
+    .unwrap()
+    .1;
+    let day4input = parseday4(include_str!("day4")).unwrap().1;
+    println!("day4pt1ex: {}", day4pt1(&day4exinput.0, &day4exinput.1));
+    println!("day4pt1: {}", day4pt1(&day4input.0, &day4input.1));
+    println!("day4pt2ex: {}", day4pt2(&day4exinput.0, &day4exinput.1));
+    println!("day4pt2: {}", day4pt2(&day4input.0, &day4input.1));
+}
+
+#[derive(Default, Clone)]
+struct BingoBoard {
+    board: Vec<Vec<BingoPlace>>,
+}
+
+impl BingoBoard {
+    fn new_from(xs: Vec<Vec<BingoPlace>>) -> Self {
+        BingoBoard { board: xs }
+    }
+
+    fn is_winner(&self) -> bool {
+        for i in 0..5 {
+            // x axis
+            if (0..5).all(|x| self.board[i][x].is_selected()) {
+                return true;
+            }
+            // y axis
+            if (0..5).all(|y| self.board[y][i].is_selected()) {
+                return true;
+            }
+        }
+        false
+    }
+
+    fn mark(&mut self, n: usize) -> bool {
+        for x in 0..5 {
+            for y in 0..5 {
+                if BingoPlace::Unselected(n) == self.board[y][x] {
+                    // XXX: I'm assuming that numbers don't repeat in a board like in normal bingo
+                    self.board[y][x].select();
+                    return true;
+                }
+            }
+        }
+        false
+    }
+
+    fn unmarked_sum(&self) -> usize {
+        self.board
+            .iter()
+            .flat_map(|row| row.iter())
+            .map(|place| {
+                if let BingoPlace::Unselected(x) = place {
+                    *x
+                } else {
+                    0
+                }
+            })
+            .sum()
+    }
+
+    fn parse(raw: &str) -> IResult<&str, BingoBoard> {
+        use nom::character::complete::{digit1, newline, space0, space1};
+        use nom::multi::separated_list1;
+
+        let mut board = Vec::with_capacity(5);
+        let mut s = raw;
+        for _ in 0..5 {
+            let (input, _) = space0(s)?;
+            let (input, row) = separated_list1(space1, digit1)(input)?;
+            let (input, _) = newline(input)?;
+
+            let vals: Vec<BingoPlace> = row
+                .iter()
+                .map(|x| BingoPlace::Unselected(x.parse::<usize>().unwrap()))
+                .collect();
+
+            board.push(vals);
+            s = input;
+        }
+
+        Ok((s, BingoBoard::new_from(board)))
+    }
+}
+
+#[derive(PartialEq, Clone)]
+enum BingoPlace {
+    Selected(usize),
+    Unselected(usize),
+}
+
+impl BingoPlace {
+    fn is_selected(&self) -> bool {
+        if let &BingoPlace::Selected(_) = self {
+            true
+        } else {
+            false
+        }
+    }
+
+    fn select(&mut self) {
+        if let BingoPlace::Unselected(v) = *self {
+            *self = BingoPlace::Selected(v)
+        }
+    }
+}
+
+fn parseday4(raw: &str) -> IResult<&str, (Vec<usize>, Vec<BingoBoard>)> {
+    use nom::bytes::complete::tag;
+    use nom::character::complete::{digit1, newline};
+    use nom::multi::{count, separated_list1};
+
+    let (input, calls) = separated_list1(tag(","), digit1)(raw)?;
+    let (input, _) = count(newline, 2)(input)?;
+    let (input, boards) = separated_list1(newline, BingoBoard::parse)(input)?;
+
+    let calls = calls.iter().map(|x| x.parse::<usize>().unwrap()).collect();
+
+    Ok((input, (calls, boards)))
+}
+
+fn day4pt1(numbers: &[usize], boards: &[BingoBoard]) -> usize {
+    let mut boards: Vec<BingoBoard> = boards.iter().map(|b| b.clone()).collect();
+    for call in numbers {
+        for b in boards.iter_mut() {
+            b.mark(*call);
+            if b.is_winner() {
+                let unselected = b.unmarked_sum();
+                return unselected * call;
+            }
+        }
+    }
+    panic!("no winner");
+}
+
+fn day4pt2(numbers: &[usize], boards: &[BingoBoard]) -> usize {
+    let mut boards: Vec<BingoBoard> = boards.iter().map(|b| b.clone()).collect();
+    for call in numbers {
+        boards.iter_mut().for_each(|b| {
+            b.mark(*call);
+        });
+        let mut unwon = boards.iter().filter(|b| !b.is_winner());
+        let last = unwon.next().unwrap();
+        if unwon.next().is_none() {
+            return day4pt1(numbers, &[last.clone()]);
+        }
+    }
+    panic!("no single final choice");
 }
 
 fn parseday3(raw: &str) -> Vec<u16> {
